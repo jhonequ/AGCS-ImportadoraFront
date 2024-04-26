8e2be3807c4317b645bcc2f83e035a2fd5e4e245
jquiceno <jquiceno@rentingcolombia.com>
2024-04-26 10:46:24 -0500

    Creando navegacion de login, y dashboard, y creando componente de las paginas

---
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -0,0 +1,12 @@
+import { Component,OnDestroy, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.css'
+})
+export class DashboardComponent {
+  userLoginOn:boolean=false;
+
+
+}
