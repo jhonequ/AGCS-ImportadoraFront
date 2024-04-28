851a845120b67cc590f2d03197a857122b45ce14
Johanna Farfán <lizeth.farfan.17@gmail.com>
2024-04-28 05:02:04 -0500

    Revert "Carga formulario producto"
    This reverts commit ca952f5497012f57da02680e9a4e3360c7ec6e78.

--- src/app/pages/productos/productos.component.ts
+++ src/app/pages/productos/productos.component.ts
@@ -1,52 +1,10 @@
 import { Component } from '@angular/core';
-import { FormControl, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-productos',
   templateUrl: './productos.component.html',
   styleUrl: './productos.component.css'
 })
-
 export class ProductosComponent {
-
-  constructor(private formBuild: FormBuilder) { }
-
-  formProductos = this.formBuild.group({
-    'idProducto': ['', Validators.required],
-    'idCliente': ['', Validators.required],
-    'nombreProducto': ['', Validators.required],
-    'fotoProducto': ['', Validators.required],
-    'precio': ['', Validators.required],
-    'fechaCompra': ['', Validators.required],
-    'estadoProducto': ['', [Validators.required]]
-  });
-
-  crearProducto() {
-    console.log(this.formProductos.value);
-  }
-
-  get idProducto() {
-    return this.formProductos.get('idProducto') as FormControl;
-  }
-
-  get idCliente() {
-    return this.formProductos.get('idCliente') as FormControl;
-  }
-
-  get nombreProducto() {
-    return this.formProductos.get('nombreProducto') as FormControl;
-  }
-
-  get precio() {
-    return this.formProductos.get('precio') as FormControl;
-  }
-
-  get fechaCompra() {
-    return this.formProductos.get('fechaCompra') as FormControl;
-  }
-
-  get estadoProducto() {
-    return this.formProductos.get('estadoProducto') as FormControl;
-  }
 
 }
