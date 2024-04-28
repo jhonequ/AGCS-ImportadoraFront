a5497adab8bad5629323fef5cee5c8d2052f3b4b
Johanna Farfán <lizeth.farfan.17@gmail.com>
2024-04-28 04:54:04 -0500

    Revert "Carga formulario importación"
    This reverts commit 0baa5987afd4090424d22ea1e9b3f980da7c6e7d.

--- src/app/pages/importaciones/importaciones.component.ts
+++ src/app/pages/importaciones/importaciones.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { FormControl, Validators, FormBuilder } from '@angular/forms';
-
 
 @Component({
   selector: 'app-importaciones',
@@ -8,45 +6,5 @@ import { FormControl, Validators, FormBuilder } from '@angular/forms';
   styleUrl: './importaciones.component.css'
 })
 export class ImportacionesComponent {
-
-  constructor(private formBuild: FormBuilder) { }
-
-  formImportaciones = this.formBuild.group({
-    'idUsuario': ['', Validators.required],
-    'idProducto': ['', Validators.required],
-    'idVenta': ['', Validators.required],
-    'estado': ['', Validators.required],
-    'fecha': ['', Validators.required],
-    'cantidad': ['', Validators.required]
-  });
-
-  crearImportacion() {
-    console.log(this.formImportaciones.value);
-  }
-
-  get idUsuario() {
-    return this.formImportaciones.get('idUsuario') as FormControl;
-  }
-
-  get idProducto() {
-    return this.formImportaciones.get('idProducto') as FormControl;
-  }
-
-  get idVenta() {
-    return this.formImportaciones.get('idVenta') as FormControl;
-  }
-  
-  get estado() {
-    return this.formImportaciones.get('estado') as FormControl;
-  }
-
-  get fecha() {
-    return this.formImportaciones.get('fecha') as FormControl;
-  }
-
-  get cantidad() {
-    return this.formImportaciones.get('cantidad') as FormControl;
-  }
-  
 
 }
